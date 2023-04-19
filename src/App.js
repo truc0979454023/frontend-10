99808e9bebe9bab6baca125d3a49aeb5b727f557
minhtruc <truc0979454023@gmail.com>
2023-04-19 21:15:48 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Account from "./components/Account";
-import Header from "./components/Header";
-import NotFound from "./components/NotFound";
+// import Header from "./components/Header";
 import Course from "./components/Course";
+import NotFound from "./components/NotFound";
 
-function App() {
+// function App() {
+//   return (
+//     <div className="app">
+//       <Header />
+//       <main className="main">
+//         <Routes>
+//           <Route path="/account" element={<Account />} />
+//           <Route path="/course" element={<Course />} />
+
+//           <Route path="*" element={<NotFound />} />
+//         </Routes>
+//       </main>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { Breadcrumb, Layout, Menu, theme } from "antd";
+import { useState } from "react";
+const { Header, Content, Footer } = Layout;
+const App = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  const navigate = useNavigate();
+
+  const [current, setCurrent] = useState("account");
+
+  const dataMenu = [
+    { key: "account", label: "Người dùng" },
+    { key: "course", label: "Câu hỏi" },
+  ];
+
+  const onClick = (e) => {
+    setCurrent(e.key);
+    navigate(e.key);
+  };
   return (
-    <div className="app">
-      <Header />
-      <main className="main">
-        <Routes>
-          <Route path="/account" element={<Account />} />
-          <Route path="/course" element={<Course />} />
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          items={dataMenu}
+          onClick={onClick}
+          selectedKeys={[current]}
+        />
+      </Header>
+      <Content
+        style={{
+          padding: "25px 50px",
+        }}
+      >
+        {/* <Breadcrumb
+          style={{
+            margin: "16px 0",
+          }}
+        >
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb> */}
+        <div
+          className="site-layout-content"
+          style={{
+            background: colorBgContainer,
+          }}
+        >
+          {/* content */}
+          <Routes>
+            <Route path="/account" element={<Account />} />
+            <Route path="/course" element={<Course />} />
 
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </main>
-    </div>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Ant Design ©2023 Created by Ant UED
+      </Footer>
+    </Layout>
   );
-}
-
+};
 export default App;
